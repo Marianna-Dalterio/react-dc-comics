24b6974b88ed46dc2a2bd60ff93a70065f92cb1c
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-09-26 13:54:37 +0200

    use map method for navbar menu

--- react-dc-comics/src/components/AppHeader.jsx
+++ react-dc-comics/src/components/AppHeader.jsx
@@ -1,73 +1,16 @@
 import "./Header.css"; //importo .css
+//importazione nominale con graffe
+import { menu } from "../data/MenuAppHeader";
+//import. di default senza graffe e dando qualsiasi nome voglio
+// import menuNavbar from "../data/MenuAppHeader";
 
 export default function () {
-    const menu = [
-        {
-            id: 1,
-            text: "Characters",
-            link: "#",
-            is_active: false
-        },
-        {
-            id: 2,
-            text: "Comics",
-            link: "#",
-            is_active: false
-        },
-        {
-            id: 3,
-            text: "Movies",
-            link: "#",
-            is_active: false
-        },
-        {
-            id: 4,
-            text: "Tv",
-            link: "#",
-            is_active: false
-        },
-        {
-            id: 5,
-            text: "Games",
-            link: "#",
-            is_active: false
-        },
-        {
-            id: 6,
-            text: "Collectibles",
-            link: "#",
-            is_active: false
-        },
-        {
-            id: 7,
-            text: "Videos",
-            link: "#",
-            is_active: false
-        },
-        {
-            id: 8,
-            text: "Fans",
-            link: "#",
-            is_active: false
-        },
-        {
-            id: 9,
-            text: "News",
-            link: "#",
-            is_active: false
-        },
-        {
-            id: 10,
-            text: "Shop",
-            link: "#",
-            is_active: false
-        }
-    ]
+
     return (<header className="header">
         <img src="/img/dc-logo.png" alt="logo" />
         <nav className="navbar">
             <ul>
-                {menu.map}
+                {menu.map(item => <li key={item.id}><a href={item.link}>{item.text}</a></li>)}
                 {/* <li > <a href="#"> Characters </a> </li>
                 <li > <a href="#">Comics</a> </li>
                 <li > <a href="#">Movies</a></li>
---
+++ react-dc-comics/src/data/MenuAppHeader.js
@@ -0,0 +1,66 @@
+//esportazione nominale dell'array 
+export const menu = [
+    {
+        id: 1,
+        text: "Characters",
+        link: "#",
+        is_active: false
+    },
+    {
+        id: 2,
+        text: "Comics",
+        link: "#",
+        is_active: false
+    },
+    {
+        id: 3,
+        text: "Movies",
+        link: "#",
+        is_active: false
+    },
+    {
+        id: 4,
+        text: "Tv",
+        link: "#",
+        is_active: false
+    },
+    {
+        id: 5,
+        text: "Games",
+        link: "#",
+        is_active: false
+    },
+    {
+        id: 6,
+        text: "Collectibles",
+        link: "#",
+        is_active: false
+    },
+    {
+        id: 7,
+        text: "Videos",
+        link: "#",
+        is_active: false
+    },
+    {
+        id: 8,
+        text: "Fans",
+        link: "#",
+        is_active: false
+    },
+    {
+        id: 9,
+        text: "News",
+        link: "#",
+        is_active: false
+    },
+    {
+        id: 10,
+        text: "Shop",
+        link: "#",
+        is_active: false
+    }
+]
+
+//esportazione di default
+//export default menu
