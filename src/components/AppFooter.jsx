45e9831b755d4dae02c62b7f1a2666b2f2b6787a
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-09-29 19:35:30 +0200

    make footer bg-img use easier on CSS rather than in AppFooter.jsx as variable

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,5 +1,5 @@
 import "./Footer.css";
-import footerbg from "../assets/img/footer-bg.jpg"; //importo background
+
 import footerlogo from "../assets/img/dc-logo-bg.png";
 //importo array di oggetti da data x la lista 
 import { menuFooter } from "../data/MenuAppFooter";
@@ -12,32 +12,44 @@ import periscope from "../assets/img/footer-periscope.png";
 
 export default function () {
     //creo oggetto di stile con la proprietà backgroundImage che usa la variabile importata sopra  
-    const footerStyle = {
-        backgroundImage: `url(${footerbg})`
 
-    };
 
-    return (<footer style={footerStyle}>
-        <div className="container">
-            <div className="row"> {/* Genitore Flexbox */}
-                {/* Logo (Posizionato assolutamente rispetto al .row) */}
-                <img src={footerlogo} alt="DC Logo" className="footer-logo" />
-                {/* Contenitore delle colonne di link (Deve essere Flex per affiancarle) */}
-                <div className="list-container">
+    return (
+        <footer className="main-footer">
+            <div className="container">
+                <div className="row"> {/* Genitore Flexbox */}
+                    {/* Logo (Posizionato assolutamente rispetto al .row) */}
+                    <img src={footerlogo} alt="DC Logo" className="footer-logo" />
+                    {/* Contenitore delle colonne di link (Deve essere Flex per affiancarle) */}
+                    <div className="list-container">
 
-                    {menuFooter.map(menu => (
-                        <div className="col" key={menu.id}><h3>{menu.title}</h3> {/* Le singole colonne */}
-                            <ul>
-                                {/* Mappa corretta sulla lista dell'elemento corrente */}
-                                {menu.list.map(list_item =>
-                                    <li key={list_item.id}>
-                                        <a href={list_item.link}>{list_item.text}</a>
-                                    </li>
-                                )}
-                            </ul>
-                        </div>
-                    ))}
+                        {menuFooter.map(menu => (
+                            <div className="col" key={menu.id}><h3>{menu.title}</h3> {/* Le singole colonne */}
+                                <ul>
+                                    {/* Mappa corretta sulla lista dell'elemento corrente */}
+                                    {menu.list.map(list_item =>
+                                        <li key={list_item.id}>
+                                            <a href={list_item.link}>{list_item.text}</a>
+                                        </li>
+                                    )}
+                                </ul>
+                            </div>
+                        ))}
 
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="greybar">
+                <button>Sign-up now!</button>
+                <div className="social-media">
+                    <span className="follow-us">follow us</span>
+                    <span><img src={facebook} alt="" /></span>
+                    <span><img src={twitter} alt="" /></span>
+                    <span><img src={youtube} alt="" /></span>
+                    <span><img src={pinterest} alt="" /></span>
+                    <span><img src={periscope} alt="" /></span>
                 </div>
 
             </div>
@@ -45,22 +57,5 @@ export default function () {
 
 
 
-        </div>
-        <div className="greybar">
-            <button>Sign-up now!</button>
-            <div className="social-media">
-                <span className="follow-us">follow us</span>
-                <span><img src={facebook} alt="" /></span>
-                <span><img src={twitter} alt="" /></span>
-                <span><img src={youtube} alt="" /></span>
-                <span><img src={pinterest} alt="" /></span>
-                <span><img src={periscope} alt="" /></span>
-            </div>
-
-        </div>
-
-
-
-
-    </footer>);
+        </footer>);
 }
